c6a45a80e4da8e96a0e66d7f2e29874f4285456b
Nick Hale <53896693+nickfurious@users.noreply.github.com>
2019-12-01 01:27:00 -0800

    Create todos.js

---
+++ todos.js
@@ -0,0 +1,12 @@
+var lis = document.querySelector("li");
+
+for(var i = 0; i < lis.length; i++){
+
+}
+firstLI.addEventListener("mouseover", function(){
+  firstLI.style.color = "green";
+});
+
+firstLI.addEventListener("mouseout", function() {
+  firstLI.style.color = "black";
+});
